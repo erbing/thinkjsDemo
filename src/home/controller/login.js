45994edfb69a3d9f0ee38d444edcab99a4308338
zigzag <447587073@qq.com>
2017-01-05 18:01:02 +0800

    login success

--- src/home/controller/login.js
+++ src/home/controller/login.js
@@ -3,12 +3,35 @@ export default class extends think.controller.base {
     async loginAction () {
         this.assign('title', '登录 - Vuejs 中文社区 ')
 
+        console.log(this.isPost(),1);
         if (this.isPost()) {
             // 用户登录成功写入 session
             let name = this.post('username');
             let psw = this.post('password');
+
+            // 定义返回错误
+            let msg = "1";
+
+            let model = this.model('user');
+            let data = await model.where({
+                username: name
+            }).find();
+            
+            if (data) {
+                if (data.password === psw) {
+                    msg = "登录成功！"
+                } else {
+                    msg = "密码错误！"
+                }
+            } else {
+                msg = '404';
+            }
+
+            this.assign('res', 'data.username');
+            this.assign('psw', 'data.password');
+            this.assign('msg', msg);
+            console.log(msg);
         }
         return this.display();
     }
-
 }
