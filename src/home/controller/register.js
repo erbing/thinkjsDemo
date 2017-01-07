a43efa19dedc7c9e3599d3bfbf660dbefb76543a
zigzga <447587073@qq.com>
2017-01-07 22:33:08 +0800

    change psw

--- src/home/controller/register.js
+++ src/home/controller/register.js
@@ -2,12 +2,12 @@ export default class extends think.controller.base {
 
     async registerAction () {
         this.assign('title', '注册 - Vuejs 中文社区 ')
-        let name = this.post('username');
-        let psw = this.post('password');
+        let name = this.post('username') ||  123456;
+        let psw = this.post('psw') || 123456;
         let model = this.model('user');
         let result = await model.add({
             username: name,
-            password: psw
+            psw: psw
         });
         this.assign('res', result)
         return this.display();
