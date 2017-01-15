f861183173bc0cff0bebf9e89b3bb2b999c874a3
zigzga <447587073@qq.com>
2017-01-15 23:48:33 +0800

    add register

--- src/home/model/register.js
+++ src/home/model/register.js
@@ -5,9 +5,9 @@ export default class extends think.model.base {
     * registerAction () {
         // 引入数据表
         let model = this.model("user");
-        // let insertUser = yield model.add({
-        //     username: '444',
-        //     password: '555'
-        // });
+        let insertUser = yield model.add({
+            username: '444',
+            password: '555'
+        });
     }
 }
